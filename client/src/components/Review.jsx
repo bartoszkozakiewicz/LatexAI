29d4b6244cbd799e9007c54b2462be9fbd59247e
Laszer271 <Laszer2710@gmail.com>
2023-07-09 18:25:21 +0200

    updated review capabilities

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -5,7 +5,7 @@ export default function Review(props){
     return(
         <div className="review-container">
             <p style={{fontWeight: "600", fontSize:'20px', marginTop:'30px'}}>REVIEW</p>
-            <div className="review-content">Content from AI
+            <div className="review-content">Content from AI<br></br>
                 {props.reviewContent}
             </div>
         </div>
